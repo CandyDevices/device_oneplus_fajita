599b948cb07849d0a49c797986072715a1a066b0
NoSpamDan <nospamdan1@gmail.com>
2021-01-09 21:29:41 -0500

    fajita: Inherit common_full_phone configs

--- candy_fajita.mk
+++ candy_fajita.mk
@@ -22,8 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
 # Inherit some common Candy stuff.
-$(call inherit-product, vendor/candy/config/common.mk)
-$(call inherit-product, vendor/candy/config/gsm.mk)
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
 PRODUCT_NAME := candy_fajita
 PRODUCT_DEVICE := fajita
